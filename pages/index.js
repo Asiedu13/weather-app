ee1df7ae067f4f939744209fa209e7fb4a22ee2f
prince <princekofasiedu@gmail.com>
2023-05-09 02:37:57 +0000

    Added Geolocation API to determine user location

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image'
-export default function Home() {
+export default function Home({city, country_code}) {
+  console.log(city)
   return (
     <>
       <Head>
@@ -10,7 +11,7 @@ export default function Home() {
         {/* Location and Date */}
         <section className="w-[fit-content] h-1/2">
               <section className='py-4'>
-                <h1 className='text-7xl font-bold pb-5 '>Lagos, NG</h1>
+                <h1 className='text-7xl font-bold pb-5 '> {city}, {country_code}</h1>
                 <p>4 May, 2023</p>
               </section>
 
@@ -34,4 +35,13 @@ export default function Home() {
    </main>
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const locationFetching = await fetch('https://api.geoapify.com/v1/ipinfo?apiKey=3f5109a61213408d95d762e8dbe73a0f')
+  const location = await locationFetching.json()
+  const city = location.city.name;
+  const country_code = location.country.iso_code
+  
+  return {props: {city, country_code}}
 }
