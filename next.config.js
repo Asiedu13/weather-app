71cd776d79698d0308b53a7d8e7fa31119097d9c
Prince Asiedu <princekofasiedu@gmail.com>
2023-10-10 21:05:35 +0000

    Updated next.config.js with rapidAPIKey

--- next.config.js
+++ next.config.js
@@ -4,13 +4,16 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '*',
-        port: '',
-        pathname: '/account123/**',
+        protocol: "https",
+        hostname: "*",
+        port: "",
+        pathname: "/account123/**",
       },
     ],
   },
-}
+  env: {
+    rapidAPIKey: "ef5512c70amsh613c620b4f53e50p1bdc18jsn6fe5b1b64bc3",
+  }
+};
 
 module.exports = nextConfig
