0c86fd42e205a29dc4d54b2b94cbb24e23696f07
Prince Asiedu <princekofasiedu@gmail.com>
2023-10-30 11:07:47 +0000

    style: updated loaders

---
+++ app/components/Loaders/main.jsx
@@ -0,0 +1,8 @@
+
+export default function Main() {
+  return (
+    <div className="w-[100vw] h-[100vh] relative flex justify-center items-center">
+      <span className="loader"></span>
+    </div>
+  );
+}
---
+++ app/history/loading.js
@@ -0,0 +1,4 @@
+import Main from "../components/Loaders/main";
+export default function Loading() {
+  return <Main />;
+}
