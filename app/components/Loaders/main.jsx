45b2e59f8280e544953ba7836b84913204c9cb55
Prince Asiedu <princekofasiedu@gmail.com>
2024-11-01 15:38:58 +0000

    misc: rename loader component

--- app/components/Loaders/main.jsx
+++ app/components/Loaders/main.jsx
@@ -1,5 +1,5 @@
 
-export default function Main() {
+export default function LoaderComponent() {
   return (
     <div className="w-[100vw] h-[100vh] relative flex justify-center items-center">
       <span className="loader"></span>
