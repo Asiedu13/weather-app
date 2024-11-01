5dbb64816a649acb4518db45556f6c241b31158d
Prince Asiedu <princekofasiedu@gmail.com>
2024-11-01 15:40:42 +0000

    feat: setup loader for server side rendering

---
+++ app/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="w-[100vw] h-[100vh] relative flex justify-center items-center">
+          <span className="loader"></span>
+        </div>
+      );
+}
