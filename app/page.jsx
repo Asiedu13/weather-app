444249a339a7e73f0bff694fc80ed0d89814b6fd
Prince Asiedu <princekofasiedu@gmail.com>
2024-11-01 15:41:00 +0000

    feat: notice to users

--- app/page.jsx
+++ app/page.jsx
@@ -1,132 +1,21 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { saveWeather } from "./utils/functions";
 
-const Home = () => {
-  const [locationData, setLocationData] = useState({});
-
-  useEffect(() => {
-    const myPromise = new Promise((resolve, reject) => {
-      if ("geolocation" in navigator) {
-        // Prompt user for permission to access their location
-        navigator.geolocation.getCurrentPosition(
-          // Success callback function
-          (position) => {
-            // Get the user's latitude and longitude coordinates
-            const lat = position.coords.latitude;
-            const lng = position.coords.longitude;
-            resolve({
-              lat,
-              lng,
-            });
-
-            // Do something with the location data, e.g. display on a map
-            console.log(`Latitude: ${lat}, longitude: ${lng}`);
-          },
-          // Error callback function
-          (error) => {
-            // Handle errors, e.g. user denied location sharing permissions
-            console.error("Error getting user location:", error);
-          }
-        );
-      } else {
-        // Geolocation is not supported by the browser
-        console.error("Geolocation is not supported by this browser.");
-      }
-    });
-    myPromise
-      .then(async (data) => {
-        const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${data.lat}%2C${data.lng}`;
-        const options = {
-          method: "GET",
-          headers: {
-            "X-RapidAPI-Key": process.env.NEXT_PUBLIC_rapidAPIKey,
-            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
-          },
-        };
-
-        try {
-          const response = await fetch(url, options);
-          const result = await response.json();
-          return result;
-        } catch (error) {
-          console.error(error);
-        }
-      })
-      .then((d) => {
-        setLocationData(d);
-      });
-  }, []);
-
-  if (!locationData.current) {
-    return (
-      <div className="w-[100vw] h-[100vh] relative flex justify-center items-center">
-        <span className="loader"></span>
-      </div>
-    );
-  }
-
+export default function Home () {
   return (
-    <>
-      <main className="w-screen h-screen flex flex-row justify-center items-center p-4 ">
-        {/* Location and Date */}
-        <section className="w-[fit-content] h-1/2">
-          <section className="py-4">
-            <h1 className="text-7xl font-bold pb-5 ">
-              {locationData.location.name}, {locationData.location.country}
-            </h1>
-            <p>{locationData.location.localtime}</p>
-          </section>
+    <main className="flex flex-col justify-center items-center h-screen">
+      <header>
+        <h2>Weather App v.0.1.0</h2>
+      </header>
 
-          {/* Temperature reading and cloud adjust */}
-          <section className="flex flex-row justify-between items-center ">
-            <div className="">
-              <span className="text-3xl font-semibold">
-                {locationData.current.feelslike_c}
-              </span>{" "}
-              <sup>&deg;C</sup>
-              <p className="text-blue-400 uppercase">
-                {locationData.current.condition.text}
-              </p>
-            </div>
-            {/* Clouds */}
-            <div>
-              <img
-                src={locationData.current.condition.icon}
-                alt="clouds"
-                width={100}
-                height={100}
-              />
-            </div>
-          </section>
-          {/* Buttons */}
-          <section className="flex justify-between items-center pt-3">
-            <button
-              onClick={() =>
-                saveWeather(
-                  locationData.location.name,
-                  locationData.location.country,
-                  locationData.location.localtime,
-                  locationData.current.feelslike_c,
-                  locationData.current.condition.text
-                )
-              }
-              className="w-[140px] h-[50px] rounded-lg bg-green-500 hover:bg-green-400"
-            >
-              Timestamp
-            </button>
-            <Link href="/history">
-              <button className="w-[140px] h-[50px] rounded-lg bg-red-500 focus:bg-red-400 hover:bg-red-400">
-                My History
-              </button>
-            </Link>
-          </section>
-        </section>
-      </main>
-    </>
+      <h2 className="text-3xl w-3/4 text-center lg:w-fit lg:text-left">Enable Location on your device</h2>
+      <span className="iconify tabler--location text-[150px] opacity-40 absolute top-20 lg:top-20 .animate-spin animate-bounce "></span>
+      <span className="iconify tabler--current-location text-[150px] opacity-20 absolute bottom-20 lg:bottom-10 left-10 animate-spin "></span>
+      <span className="iconify fluent--weather-rain-snow-20-regular text-[250px] opacity-20 absolute bottom-60 -right-20 lg:right-20 lg:bottom-60 "></span>
+
+      <a href="/weather" className="bg-white text-primary p-3 rounded-md w-2/6 text-center mt-2 flex gap-2 items-center lg:w-fit lg:px-10">Next <span className="iconify solar--arrow-right-line-duotone text-lg relative"></span> </a>
+    </main>
   );
 };
 
-export default Home;
